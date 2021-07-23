76b70d8d20ad1a2548965746b984b1627d711be5
Simon <simonwatson176@gmail.com>
2021-07-23 17:04:44 +0100

    mvp met

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    console.log(Playlist);
+document.addEventListener('DOMContentLoaded', () => {
+    // console.log(Playlist);
     const newItemform = document.querySelector('#new-item-form');
     newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+    const deleteAllButton = document.querySelector('#delete-all');
+    deleteAllButton.addEventListener('click', handDeleteAllClick);
 })
 
-const handleNewItemFormSubmit = function(event) {
+const handleNewItemFormSubmit = function (event) {
     event.preventDefault();
 
     const listeningListItem = createListeningListItem(event.target);
@@ -18,6 +21,22 @@ const createListeningListItem = function(form) {
     const listeningListItem = document.createElement('li');
     listeningListItem.classList.add('listening-list-item');
 
+    const title = document.createElement('h3');
+    title.textContent = form.title.value;
+    listeningListItem.appendChild(title);
+
+    const artist = document.createElement('h3');
+    artist.textContent = form.artist.value;
+    listeningListItem.appendChild(artist);
+
+    const category = document.createElement('p');
+    category.textContent = form.category.value;
+    listeningListItem.appendChild(category);
 
     return listeningListItem;
+}
+
+const handDeleteAllClick = function() {
+    const listeningList = document.querySelector('#listening-list');
+    listeningList.innerHTML = '';
 }
